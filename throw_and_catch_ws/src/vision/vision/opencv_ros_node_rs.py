8466659d0166a8c7a3e8d31e17770e1a0d1edc4b
Ashik ubuntu legion <ashikislam232@gmail.com>
2026-02-07 23:26:37 -0800

    updated to save frames every 30th from realsense camera

--- throw_and_catch_ws/src/vision/vision/opencv_ros_node_rs.py
+++ throw_and_catch_ws/src/vision/vision/opencv_ros_node_rs.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+from pathlib import Path
 
 
 class OpenCVROSNode(Node):
@@ -11,8 +12,12 @@ class OpenCVROSNode(Node):
 
         self.bridge = CvBridge()
 
-        # Common RealSense color topic when launched with rs_launch.py:
-        topic = '/camera/camera/color/image_raw'  # change if your topic differs
+        self.images_dir = Path(__file__).resolve().parent / 'd455_images' /'fps60'
+        self.images_dir.mkdir(parents=True, exist_ok=True)
+        self.img_index = 0
+
+        # RealSense color topic when launched with rs_launch.py:
+        topic = '/camera/camera/color/image_raw'  
         self.sub = self.create_subscription(Image, topic, self.cb, 10)
 
         self.get_logger().info(f"Subscribing to: {topic}")
@@ -20,10 +25,18 @@ class OpenCVROSNode(Node):
 
     def cb(self, msg: Image):
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        cv2.imshow("realsense_color", frame)
 
-        if (cv2.waitKey(1) & 0xFF) == ord('q'):
-            rclpy.shutdown()
+        if self.img_index % 30 == 0:
+            image_path = self.images_dir / f'img_60fps_{self.img_index}.jpg'
+            self.get_logger().info(f"Saving image to: {image_path}")
+            cv2.imwrite(str(image_path), frame)
+        
+        self.img_index += 1
+        
+        # cv2.imshow("realsense_color", frame)
+
+        # if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        #     rclpy.shutdown()
 
     def destroy_node(self):
         try:
