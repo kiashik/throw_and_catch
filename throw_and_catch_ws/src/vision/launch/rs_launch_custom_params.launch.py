4eb95d9ad6f9c5aa0a7b15288abe77419d34aa43
Ashik ubuntu legion <ashikislam232@gmail.com>
2026-02-11 23:26:14 -0800

    chnaged the format to match teh example given by realsense for how to launch a launch file from another launch file

--- throw_and_catch_ws/src/vision/launch/rs_launch_custom_params.launch.py
+++ throw_and_catch_ws/src/vision/launch/rs_launch_custom_params.launch.py
@@ -1,66 +1,50 @@
+"""
+I'm following the example from the ROS 2 launch documentation to create a launch 
+file that loads custom parameters from a YAML file for the RealSense camera. see
+the file "realsense-ros/realsense2_camera/examples/launch_params_from_file"
+
+
+Purpose: launch the camera bringup with a set of params that work for our project.
+This way we can "disable" all unnecessary data from the camera to save bandwidth.
+
+## Syntax for defining params in YAML file
+```
+param1: value
+param2: value
+```
+"""
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+import launch_ros.actions
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
+import sys
+import pathlib
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
+import os
+from ament_index_python.packages import get_package_share_directory
+sys.path.append(os.path.join(get_package_share_directory('realsense2_camera'), 'launch'))
+import rs_launch
+
+# these are the custom parameters that we are using for our throw and catch project.
+# note that the last parameter is the config file that lists most of the parameters
+local_parameters = [{'name': 'camera_name',         'default': 'camera', 'description': 'camera unique name'},
+                    {'name': 'camera_namespace',    'default': 'camera', 'description': 'camera namespace'},
+                    {'name': 'config_file',         'default': os.path.join(get_package_share_directory('vision'), 'config', 'rs_launch_config.yaml'), 'description': 'yaml config file for realsense camera'},
+
+                    ]
+
+def set_configurable_parameters(local_params):
+    return dict([(param['name'], LaunchConfiguration(param['name'])) for param in local_params])
 
 
 def generate_launch_description():
-    # Your defaults (change these!)
-    declared_args = [
-        DeclareLaunchArgument('camera_name', default_value='camera'),
-        DeclareLaunchArgument('camera_namespace', default_value='camera'),
-
-        DeclareLaunchArgument('enable_depth', default_value='false'),
-        DeclareLaunchArgument('enable_sync', default_value='false'),
-        DeclareLaunchArgument('enable_rgbd', default_value='false'),
-
-        # Useful for perception pipelines
-        DeclareLaunchArgument('align_depth.enable', default_value='false'),
-        
-        DeclareLaunchArgument('spatial_filter.enable', default_value='false'),
-        DeclareLaunchArgument('temporal_filter.enable', default_value='false'),
-        DeclareLaunchArgument('disparity_filter.enable', default_value='false'),
-        DeclareLaunchArgument('hole_filling_filter.enable', default_value='false'),
-
-        # Profiles (format is width,height,fps)
-        DeclareLaunchArgument('rgb_camera.color_profile', default_value='640,480,60'),
-        DeclareLaunchArgument('depth_module.depth_profile', default_value='640,480,60'),
-
-        DeclareLaunchArgument('output', default_value='screen'),
-        DeclareLaunchArgument('log_level', default_value='info'),
-    ]
-
-    realsense_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare('realsense2_camera'),
-                'launch',
-                'rs_launch.py',
-            ])
-        ),
-        launch_arguments={
-            # Forward your args into rs_launch.py
-            'camera_name': LaunchConfiguration('camera_name'),
-            'camera_namespace': LaunchConfiguration('camera_namespace'),
-
-            'enable_depth': LaunchConfiguration('enable_depth'),
-            'enable_sync': LaunchConfiguration('enable_sync'),
-            'enable_rgbd': LaunchConfiguration('enable_rgbd'),
-
-            'align_depth.enable': LaunchConfiguration('align_depth.enable'),
-            
-            'spatial_filter.enable': LaunchConfiguration('spatial_filter.enable'),
-            'temporal_filter.enable': LaunchConfiguration('temporal_filter.enable'),
-            'disparity_filter.enable': LaunchConfiguration('disparity_filter.enable'),
-            'hole_filling_filter.enable': LaunchConfiguration('hole_filling_filter.enable'),
-
-            'rgb_camera.color_profile': LaunchConfiguration('rgb_camera.color_profile'),
-            'depth_module.depth_profile': LaunchConfiguration('depth_module.depth_profile'),
-
-            'output': LaunchConfiguration('output'),
-            'log_level': LaunchConfiguration('log_level'),
-        }.items()
-    )
-
-    return LaunchDescription(declared_args + [realsense_launch])
+    params = rs_launch.configurable_parameters
+    return LaunchDescription(
+        rs_launch.declare_configurable_parameters(local_parameters) +
+        rs_launch.declare_configurable_parameters(params) + 
+        [
+        OpaqueFunction(function=rs_launch.launch_setup,
+                kwargs = {'params' : set_configurable_parameters(params)}
+        )
+    ])
