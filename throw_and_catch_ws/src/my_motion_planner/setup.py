d81f956bb11fe8b2315dae0d65fb011ca67d8ce0
Ashik ubuntu legion <ashikislam232@gmail.com>
2026-03-03 16:12:49 -0800

    probably added entry points for config and launch files

--- throw_and_catch_ws/src/my_motion_planner/setup.py
+++ throw_and_catch_ws/src/my_motion_planner/setup.py
@@ -14,6 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -30,6 +31,8 @@ setup(
         'console_scripts': [
             "convert_pose_cam_to_rob = my_motion_planner.convert_pose_cam_to_rob:main",
             "track_ball = my_motion_planner.track_ball:main",
+            "track_ball_servo = my_motion_planner.track_ball_servo:main",
+            "fake_pub_delta_twist = my_motion_planner.fake_pub_delta_twist:main",
         ],
     },
 )
